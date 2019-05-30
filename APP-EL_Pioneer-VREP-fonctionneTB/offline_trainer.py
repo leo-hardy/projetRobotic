35a941c4128dc54a1eeaa91e844e8721276aa830
maximelatge <maxime.latge@gmail.com>
2019-05-30 16:35:06 +0200

    Some modifications

--- APP-EL_Pioneer-VREP-fonctionneTB/offline_trainer.py
+++ APP-EL_Pioneer-VREP-fonctionneTB/offline_trainer.py
@@ -9,7 +9,7 @@ theta = 0
 M = 1  # vitesse angulaire max des roues en sortie
 position = [0, 0, 0]
 pi = math.pi
-sample_position = []  # liste des positions cibles (ds le repere du robot) finales
+sample_position = []  # liste des positions cibles (ds le repere du robot) finales, [x,y,orientation_cible]
 sample_command = []  # liste des commandes des roues finales
 
 # generation de la base de test normalisee
@@ -17,12 +17,12 @@ sample_command = []  # liste des commandes des roues finales
 for i in [1 / 4, 1 / 2, 3 / 4, 1]:
     for j in [1 / 4, 1 / 2, 3 / 4, 1]:
         for theta in [-pi, -(3 * pi) / 4, -pi / 2, -pi / 4, 0, pi / 4, pi / 2, 3 * pi / 4]:
-            sample_position += [[1 / i, 0, theta / (2 * pi)],  # 1
-                                [1 * i, 1 * j, theta / (2 * pi)],  # 2
-                                [-1 * i, 1 * j, theta / (2 * pi)],  # 3
-                                [-1 * i, 0, theta / (2 * pi)],  # 4
-                                [-1 * i, -1 * j, theta / (2 * pi)],  # 5
-                                [1 * i, -1 * j, theta / (2 * pi)]]  # 6
+            sample_position += [[1 * i, 0, theta / (pi)],  # 1
+                                [1 * i, 1 * j, theta / (pi)],  # 2
+                                [-1 * i, 1 * j, theta / (pi)],  # 3
+                                [-1 * i, 0, theta / (pi)],  # 4
+                                [-1 * i, -1 * j, theta / (pi)],  # 5
+                                [1 * i, -1 * j, theta / (pi)]]  # 6
 
             sample_command += [[1, 1],  # 1
                                [-1, 1],  # 2
@@ -33,13 +33,13 @@ for i in [1 / 4, 1 / 2, 3 / 4, 1]:
 
 # boucle pour generer les combinaisons 7 et 8
 for k in [(3 / 4), (1 / 2), (1 / 4)]:
-    sample_position += [[0, 0, -1 * (pi * k) / (2 * pi)],  # 7
-                        [0, 0, pi * k / (2 * pi)]]  # 8
+    sample_position += [[0, 0, -1 * (pi * k) / (pi)],  # 7
+                        [0, 0, pi * k / (pi)]]  # 8
 
     sample_command += [[1, -1],  # 7
                        [-1, 1]]  # 8
 
-# g�n�ration du point final   #9
+# generation du point final   #9
 sample_position += [[0, 0, 0]]  # len(sample_position) = 775
 sample_command += [[0, 0]]
 
@@ -55,7 +55,7 @@ sample_command += [[0, 0]]
 # [0,0,pi*k],#8
 # [0,0,0]]#9
 
-# liste des sorties d�sir�es
+# liste des sorties desirees
 # L_q = [[M,M],#1
 # [M,-M],#2
 # [-M,M],#3
@@ -84,16 +84,16 @@ class OfflineTrainer:
         self.robot = robot
         self.network = NN
 
-        self.alpha = [1 / (2 * L), 1 / (2 * L),
-                      1 / (2 * math.pi)]  # normalition avec limite du monde cartesien = -3m � + 3m
+        self.alpha = [1 / (2 * L), 1 / (2 * L), 1 / (2 * math.pi)]  # normalition avec limite du monde cartesien = -3m � + 3m
 
-    # pourquoi pas avec 1/L ? sinon on va de -0.5 a 0.5.
+    # pourquoi pas avec 1/L ? sinon on va de -0.5 a 0.5, de même avec 1/2pi !
 
     # il faut que quand training soit True l'apprentissage soit lance
     # d'un bloc. Il faut qu'il soit False qd le robot bouge
     def train(self, target):
 
         somme_erreur_av = [len(sample_position) * 4, len(sample_position) * 4]  # erreur maximum,
+        print("\nsomme_erreure maximale (calcul initial) =" + str(somme_erreur_av[0]) + "," + str(somme_erreur_av[1]) + "]")  
         n_it = 0
 
         while self.training:
@@ -103,13 +103,12 @@ class OfflineTrainer:
 
             for k in range(100):
                 for i in range(len(sample_position)):
-                    command = self.network.runNN(
-                        sample_position[i])  # propage erreur et calcul vitesses roues instant t
+                    command = self.network.runNN(sample_position[i])  # propage erreur et calcule la  vitesse des roues instant t
                     erreur = [(command[0] - sample_command[i][0]) ** 2, (command[1] - sample_command[i][1]) ** 2]
                     somme_erreur[0] += erreur[0]
                     somme_erreur[1] += erreur[1]
 
-                # print("somme_erreur_av = ["+str(somme_erreur_av[0])+","+str(somme_erreur_av[1])+"]")
+                print("A l'itération " + str(n_it) + ", somme_erreur = ["+str(somme_erreur[0])+","+str(somme_erreur[1])+"]")
                 # print("somme_erreur = ["+str(somme_erreur[0])+","+str(somme_erreur[1])+"]")
 
                 grad = [0, 0]
@@ -123,8 +122,9 @@ class OfflineTrainer:
                 n_it += 1
 
             self.training = False
-            print("somme_erreur = [" + str(somme_erreur[0]) + "," + str(somme_erreur[1]) + "]")
-            print("Training done after " + str(n_it) + " iterations !")
+            
+            print("\nsomme_erreur finale = [" + str(somme_erreur[0]) + "," + str(somme_erreur[1]) + "]")
+            print("\nTraining done after " + str(n_it) + " iterations !")
 
             # version avec arret quand l'erreure augmente
             # if (somme_erreur[0]+somme_erreur[1]) < (somme_erreur_av[0]+somme_erreur_av[1]) :
@@ -144,6 +144,7 @@ class OfflineTrainer:
 
             network_input = [0, 0, 0]
 
+            # calcul de la position relative de la cible dans le referentiel du robot
             network_input[0] = ((target[0] - position[0]) * math.cos(position[2]) - (
                     target[1] - position[1]) * math.sin(position[2])) * self.alpha[0]
             network_input[1] = ((target[0] - position[0]) * math.sin(position[2]) - (
@@ -152,6 +153,8 @@ class OfflineTrainer:
 
             command = self.network.runNN(network_input)  # propage erreur et calcul vitesses roues instant t
 
+
+            #faut il mmultiplier la commande par certains paramètres pour "dénormaliser la commande" ?
             self.robot.set_motor_velocity(command)  # applique vitesses roues instant t,
             time.sleep(0.050)  # attend delta t
 
